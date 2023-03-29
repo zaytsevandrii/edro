1b82dab591bf112b776ffc43dab567599b6e93c1
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-03-29 18:06:27 +0100

    add index and changable styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import { useEffect } from 'react';
+import {BrowserRouter, Route, Routes, useLocation} from 'react-router-dom'
+import Home from './scenes/home/Home';
+
+const ScrollToTop = ()=>{
+  const {pathname} = useLocation()
+
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  },[pathname])
+
+  return null
+}
 
 function App() {
   return (
     <div className="app">
-     dsd
-     <h2>this is my text</h2>
-     <p>jdjsdjsj</p>
+      <BrowserRouter>
+      <ScrollToTop/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
